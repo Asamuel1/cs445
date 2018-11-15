5a159c7e2f45cceeb5b26c5dc62815c37d1972b1
Anthony Samuel <anthony.b.samuel@hotmail.com>
2018-11-15 01:27:28 +0000

    Updated API project

--- Server.js
+++ Server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
-const request = require("request");
-const response = require("respone");
+app.use(express.json());
+app.get( '/reveiws/:reveiwid' , (req,res) => {
+    
+    res.send(req.query.reviewid);
+    
+});
 
-app.get( '/reveiws/:reveiwid' , (request,response));
-app.listen(7000);
-console.log("Listening on port 7000..");
+app.get('https://server/review/:n/:stars', (req,res) => {
+    
+    res.send(req.query.stars);
+});
+app.get('https://server/review/:n/:from_date/:to_date', (req,res) => {
+    
+    res.send(req.query.to_date);
+});
+app.post('https://server/review/:reviewid');
+app.put('https://server/review/:reviewid');
+app.delete('https://server/review/:reviewid');
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log('Listening on port 8080..');
